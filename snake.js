49e40a1b62fadbfbb1831306009048042c52cf72
Jorkoh <kohru95@gmail.com>
2023-03-31 14:18:41 +0200

    Added Power Button for snake screen

--- snake.js
+++ snake.js
@@ -1,3 +1,19 @@
+function TurnOffOn() {
+    var TurnOn = document.getElementById('Button_on');
+    var TurnOff = document.getElementById('Button_off');
+
+    /* Right now you can click in all the img, it should not behave that way */
+
+    if (TurnOn.hasAttribute('hidden')) {
+        TurnOn.removeAttribute('hidden');
+        TurnOff.setAttribute('hidden', true);
+    } else {
+        TurnOff.removeAttribute('hidden');
+        TurnOn.setAttribute('hidden', true);
+    }
+
+}
+
 setTimeout(drawNintendo, 1000);
 
 

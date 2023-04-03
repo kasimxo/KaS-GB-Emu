58fba183a42288fe23d8628f99f696a7d8fbacd7
kasimxo <kasimxo@gmail.com>
2023-04-03 17:10:05 +0200

    Improved Turning On experience

--- snake.js
+++ snake.js
@@ -1,6 +1,9 @@
 var PowerButton;
-var textPosition = 0;
+var logoPosition = 0;
 var DrawNintendoID;
+var PlayerX;
+var PlayerY;
+
 function TurnOffOn() {
     PowerButton = document.getElementById('Button_power');
     let PowerButtonState = PowerButton.getAttribute("data-state");
@@ -13,7 +16,6 @@ function TurnOffOn() {
         PowerButton.style.left = '40px';
         PowerButton.setAttribute("data-state","off"); 
     }
-
 }
 
 function TurnOn() {
@@ -25,17 +27,15 @@ function drawNintendo() {
     clearCanvas();
     var canvas = document.getElementById("play");
     var ctx = canvas.getContext("2d");
-
-
-    ctx.font='50px Calibri';
-    ctx.fillStyle = "black";
-    ctx.textAlign='center';
-    ctx.fillText('Nintendo',(canvas.width/2),textPosition); 
-
-    textPosition++;
+    var NintendoLogo = new Image();
+    NintendoLogo.src = 'imgs/gameBoy/Nintendo_logo.png';
+    ctx.drawImage(NintendoLogo,16,logoPosition-(NintendoLogo.height/2),NintendoLogo.width/2,NintendoLogo.height/2);
     
-    if(textPosition >= canvas.height/2) {
+    logoPosition++;
+    
+    if(logoPosition >= canvas.height/2) {
         window.clearInterval(DrawNintendoID);
+        setTimeout(start,"1000");
     }
     
 }
@@ -43,6 +43,13 @@ function drawNintendo() {
 function clearCanvas() {
     var canvas = document.getElementById("play");
     var ctx = canvas.getContext("2d");
-
     ctx.clearRect(0,0,canvas.width,canvas.height);
+}
+
+function start() {
+    clearCanvas();
+    var canvas = document.getElementById("play");
+    var ctx = canvas.getContext("2d");
+
+    
 }
